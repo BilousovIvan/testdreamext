4032f6809a76119bf520e60b2a4b950d95fe1773
JohnBilousov <beloublog@gmail.com>
2022-12-18 23:05:13 +0200

    add Snack bar and header

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,24 +1,55 @@
-import {FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import Post from '../components/Post';
+import {useLinkTo} from '@react-navigation/native';
+import ButtonsComponent from '../components/UI/ButtonsComponent';
+import Snackbar from 'react-native-snackbar';
 
 const HomeScreen = () => {
+  const linkTo = useLinkTo();
+
   const [post, setPost] = useState([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts/')
+    fetch('https://jsonplaceholder.typicode.com9/posts')
       .then(response => response.json())
       .then(json => setPost(json))
-      .catch(error => {});
+      .catch(error => {
+        Snackbar.show({
+          text: 'Error ',
+          duration: Snackbar.LENGTH_LONG,
+        });
+      });
   }, []);
   return (
     <View style={styles.container}>
       <SafeAreaView>
-        <FlatList
-          data={post}
-          renderItem={Post}
-          keyExtractor={index => index.id}
-        />
+        <View style={styles.header}>
+          <ButtonsComponent
+            title="Log Out"
+            onPress={() => linkTo({screen: 'Login'})}
+            width={90}
+          />
+        </View>
+
+        {post.length >= 1 ? (
+          <FlatList
+            data={post}
+            renderItem={Post}
+            keyExtractor={index => index.id}
+          />
+        ) : (
+          <View style={styles.preloader}>
+            <ActivityIndicator size={'large'} />
+          </View>
+        )}
       </SafeAreaView>
     </View>
   );
@@ -30,5 +61,14 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     height: '100%',
+  },
+  header: {
+    alignItems: 'flex-end',
+    paddingHorizontal: 24,
+    height: '7%',
+  },
+  preloader: {
+    height: '93%',
+    justifyContent: 'center',
   },
 });
